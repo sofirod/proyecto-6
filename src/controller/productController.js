462b5330daa885401e5f19663f65f893670688f9
sofirod <sofirodbas@gmail.com>
2025-06-09 18:43:22 -0400

    Corrige función deleteProduct

--- src/controller/productController.js
+++ src/controller/productController.js
@@ -49,7 +49,7 @@ const deleteProduct = async (req, res) => {
         if (!deletedProduct) {
             return res.status(404).json({ message: "Producto no encontrado" });
         }} catch (error) {      
-        res.status(500).json({ message: "Error al eliminar el producto" });
+       return res.status(500).json({ message: "Error al eliminar el producto" });
     }  }
 module.exports = {
     getProducts,
